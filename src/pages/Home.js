19393a7127eb97a2f9be8c796488754d8c23f28c
gffuma <fumagalli.gf@gmail.com>
2017-11-30 16:55:06 +0100

    Added bootstrap4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ class Home extends PureComponent {
   render() {
     return (
       <div>
+        <div className='p-2'>
+          <button className='btn btn-primary'>BUTTON</button>
+        </div>
         <Motion defaultStyle={{ top: -60 }} style={{ top: spring(100, presets.wobbly) }}>
           {({ top }) => (
             <div style={{ textAlign: 'center', marginTop: top }}>
