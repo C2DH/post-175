0045388cf5f95887f17ad7fcc4d94b2c5dea5d25
Mauro Bianchi <bianchimro@gmail.com>
2017-12-04 13:26:46 +0100

    experimenting with layout

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -4,14 +4,32 @@ import TopBar from '../components/TopBar'
 class Timeline extends PureComponent {
   render() {
     return (
-      <div className='container-fluid p-0'>
-        <div className='row'>
-          <div className='col-md-3'>
+      <div className='h-100vh d-flex flex-column'>
+        <div className='row no-gutters flex-1'>
+          <div className='col-md-3 bg-info'>
             <TopBar title={'TIMELINE'} />
             1830 - 1880
           </div>
-          <div className='col-md-9'>
-            ...
+          <div className='col-md-9 bg-light d-flex flex-column'>
+
+            {/* top timeline */}
+            <div className="align-self-start bg-warning w-100 " style={{height:52}}>
+
+            </div>
+
+
+            <div className="bg-secondary align-self-stretch flex-1 position-relative" style={{overflow:'hidden'}}>
+
+              <div className="position-absolute bg-primary" style={{width:30000, height:100, transform:'translate(100px, 0)'}}>
+
+              </div>
+
+            </div>
+
+            {/* bottom timeline */}
+            <div className="align-self-end bg-warning w-100" style={{height:100}}>
+
+            </div>
           </div>
         </div>
       </div>
