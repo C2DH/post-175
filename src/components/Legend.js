9a07d4c58dbfb4a860650f0217005114fd4c3401
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-12-04 17:15:28 +0100

    Legend componenet & layout for MapPage

---
+++ src/components/Legend.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class Legend extends PureComponent {
+  render () {
+    return (
+      <div className="col-md-3 bg-info d-flex flex-column flex-1">
+        ciao
+      </div>
+    )
+  }
+}
+
+export default Legend
