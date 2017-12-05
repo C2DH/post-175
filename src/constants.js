c7a399e2a9ae10ed866cd38441ca77f2afe421a4
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-12-05 17:22:25 +0100

    adding constants

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+// --Evenement: #ed6505;
+// --Legislation: #186a0f;
+// --services: #55f7df;
+// --cooperation: #2933ff;
+// --personell: #fdd00c;
+// --transport: #b413da;
+// --batiments: #ff3e4c;
+// --Technologie: #13d436;
+
+
+export const EVENT_COLORS =  {
+  event: '#ed6505',
+  legislation: '#186a0f',
+  service: '#55f7df',
+  cooperation: '#2933ff',
+  staff: '#fdd00c',
+  transport: '#b413da',
+  building: '#ff3e4c',
+  technology: '#13d436',
+}
