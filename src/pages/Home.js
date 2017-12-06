54c662da5e58b603784592f0f23861102b001771
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-12-06 12:39:52 +0100

    Home page layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,8 @@ class Home extends PureComponent {
   render() {
     const { url } = this.props
     return (
-      <div>
-        <Motion defaultStyle={{ top: -60 }} style={{ top: spring(100, presets.wobbly) }}>
+      <div className="h-100vh d-flex flex-column bg-dark">
+        {/* <Motion defaultStyle={{ top: -60 }} style={{ top: spring(100, presets.wobbly) }}>
           {({ top }) => (
             <div style={{ marginTop: top }} className="pl-3">
               <h1 className="display-3">175 Joer Post</h1>
@@ -21,11 +21,48 @@ class Home extends PureComponent {
               <div className="mt-3"><Link to={url('/about')}>About</Link></div>
             </div>
           )}
-        </Motion>
+        </Motion> */}
+        <div className='row no-gutters flex-1 w-100 bg-info'>
+          <h1 className="display-2">Images container</h1>
+        </div>
+        <div className='row no-gutters flex-1 w-100 d-flex p-3 bg-black'>
+          <div className="d-flex h-100 flex-1">
+            <h5 style={{fontSize: 28}}><b>175</b> Joer Post</h5>
+          </div>
+          <div className="d-flex h-100 flex-1 flex-column">
+            <p style={{fontSize: 24, fontWeight: 300}}>Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus nibh.
+              Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
+              <a><u>Read more</u></a>
+          </div>
+          <div className="d-flex h-100 justify-content-end" style={{flex: 1.5}}>
+            <div className="d-flex flex-column">
+              <div className="p-3 border mb-1">
+                <Link to={url('/timeline')}>
+                  <span style={{fontSize: 24, fontWeight: 300}} className="text-light d-flex d-inline-flex justify-content-between align-items-center w-100">
+                    <span className="ml-3">Discover the Timeline</span>
+                    <i className="material-icons mr-3 ml-2">arrow_forward</i>
+                  </span>
+                </Link>
+              </div>
+              <div className="p-3 border mt-1">
+                <Link to={url('/map')}>
+                  <span style={{fontSize: 24, fontWeight: 300}} className="text-light d-flex d-inline-flex justify-content-between align-items-center w-100">
+                    <span className="ml-3">Explore the Map</span>
+                    <i className="material-icons mr-3 ml-2">arrow_forward</i>
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='row no-gutters w-100 fixed-bottom d-flex align-items-center bg-opaque-black' style={{height: 60}}>
+          <p className="m-0 pl-3">A project by the Luxembourg Centre for the Contemporary and Digital History (CD2H)</p>
+        </div>
       </div>
     )
   }
 }
+
 
 const mapStateToProps = state => ({
   url: getMakeLangUrl(state),
