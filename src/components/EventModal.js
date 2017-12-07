85358bd034e3fa2420f7b90bb4101ce87c65760e
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-12-07 17:24:33 +0100

    change modals buttons

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -15,13 +15,32 @@ const EventType = ({eventColor, eventType}) => (
     </svg>
     <span className="font-12 text-capitalize" style={{color: eventColor}}>{eventType}</span>
   </div>
-
 )
 
+const EventsControl = ({hasPrev, hasNext, goNext, goPrev}) => (
+  <div className="w-100 d-inline-flex justify-content-between">
+    <div
+      className="p1 d-inline-flex event-modal-control"
+      onClick={goPrev}
+      style={!hasPrev ? {poiterEvents: 'none', color: '#fff'} : undefined}
+      >
+      <i className="material-icons">arrow_back</i>
+      <span className="pb-2 ml-2">Previous event</span>
+    </div>
+    <div
+      className="p1 d-inline-flex event-modal-control"
+      onClick={goNext}
+      style={!hasNext ? {poiterEvents: 'none', color: '#fff'} : undefined}
+      >
+      <span className="pb-2 mr-2">Next event</span>
+      <i className="material-icons">arrow_forward</i>
+    </div>
+  </div>
+)
 
 class EventModal extends PureComponent {
   render() {
-    const { event, onClose } = this.props
+    const { event, onClose, hasPrev, hasNext, goNext, goPrev } = this.props
     return (
       <div className="bg-black p-3 fixed-top fixed-bottom">
         <div className="container h-100 d-flex text-dark">
@@ -36,7 +55,7 @@ class EventModal extends PureComponent {
             <div className="p-3 mt-3 overflow-auto" style={{flex: 2}}>
               <p>{event.data.description}</p>
             </div>
-            <div className="culo w-100 d-inline-flex justify-content-between">
+            {/* <div className="culo w-100 d-inline-flex justify-content-between">
               <div className="pl-1 pb-1 d-inline-flex">
                 <i className="material-icons">arrow_back</i>
                 <span className="pb-2 ml-2">Previous event</span>
@@ -45,7 +64,13 @@ class EventModal extends PureComponent {
                 <span className="pb-2 mr-2">Next event</span>
                 <i className="material-icons">arrow_forward</i>
               </div>
-            </div>
+            </div> */}
+            <EventsControl
+              hasNext={hasNext}
+              hasPrev={hasPrev}
+              goNext={goNext}
+              goPrev={goPrev}
+            />
           </div>
           <div className="h-100 flex-column d-flex" style={{flex: 2.55, backgroundColor: '#313030'}}>
             <div className="w-100 bg-info" style={{height: 57}}>
