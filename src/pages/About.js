9e923a427b09f9b504f202e89e03eaa56ee8f144
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-12-07 12:40:05 +0100

    working on EventModal

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react'
+import EventModal from '../components/EventModal'
 
 class About extends PureComponent {
   render() {
     return (
       <div>
-        <h1>About</h1>
+        <EventModal />
       </div>
     )
   }
