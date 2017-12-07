fdfc4129b201a8d95c70476fbe91c35f2848002d
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-12-07 12:50:46 +0100

    working on modal

---
+++ src/components/EventModal.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+
+class EventModal extends PureComponent {
+  render() {
+    return (
+      <div className="bg-dark p-3 fixed-top fixed-bottom" >
+        <div className="container h-100">
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EventModal
