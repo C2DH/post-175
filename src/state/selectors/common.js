a9ce9fa511882aa3bda846f4112767281648d3b8
gffuma <fumagalli.gf@gmail.com>
2017-12-12 12:12:13 +0100

    Translate event documents

--- src/state/selectors/common.js
+++ src/state/selectors/common.js
@@ -1,12 +1,15 @@
-import { mapValues, isPlainObject } from 'lodash'
+import { mapValues, isPlainObject, isArray } from 'lodash'
 
 export const translateDoc = (doc, langCode) => {
-  const transObj = (obj, key, sourceObj) => {
+  const transObj = (obj) => {
     if (isPlainObject(obj)) {
       if (typeof obj[langCode] !== 'undefined') {
         return obj[langCode]
       }
       return mapValues(obj, transObj)
+    }
+    if (isArray(obj)) {
+      return obj.map(transObj)
     }
     return obj
   }
