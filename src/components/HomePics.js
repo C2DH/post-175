3f20b573ea020a6749b68aa537f4e694f9f39ddf
gffuma <fumagalli.gf@gmail.com>
2017-12-14 19:25:50 +0100

    A decent home pics...

--- src/components/HomePics.js
+++ src/components/HomePics.js
@@ -2,25 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { range, sumBy, sum, max } from 'lodash'
 import { StaggeredMotion, Motion, spring } from 'react-motion'
-import { scaleLinear } from 'd3-scale'
+import { scaleLinear, scalePow } from 'd3-scale'
 const NUM_PICS = 50
+const MAX_DELTA = 100
 
 export default class HomePics extends React.PureComponent {
 
   state = {
-    width: 0,
     x: null,
-    flexs: range(NUM_PICS).map(()=>1),
+    positions: [],
+    width: 0,
   }
 
   componentDidMount(){
     const node = ReactDOM.findDOMNode(this)
     const width = node.offsetWidth
-    this.setState({width})
+    const itemWidth = width ? width / NUM_PICS : 0
+    const positions = range(NUM_PICS).map((pic, i) => {
+      const pos = i * itemWidth
+      return [pos, pos + itemWidth]
+    })
+    this.setState({width, positions, initialPositions: positions})
   }
-
-  flexScale = scaleLinear().range([NUM_PICS/100, NUM_PICS/10, NUM_PICS])
-
 
   getColor = (i) => {
     const itemWidth = Math.round(255.0 / NUM_PICS)
@@ -34,122 +37,90 @@ export default class HomePics extends React.PureComponent {
 
   handleMouseMove = (e) => {
     // console.log(e.clientX)
-    const { width, flexs } = this.state
-    const itemWidth = width ? width / NUM_PICS : 0
-    // console.log(e.clientX / itemWidth)
-    const index = Math.round(e.clientX / itemWidth)
+    const { width, initialPositions } = this.state
+    const leftScale = scaleLinear().domain([0, e.clientX]).range([MAX_DELTA, 0])
+    const rightScale = scaleLinear().domain([0, width - e.clientX]).range([MAX_DELTA, 0])
 
-    const totalFlex = sum(flexs)
-    const maxFlex = max(flexs)
-
-    // this.setState({x: e.clientX / itemWidth})
-    const flexUnitWidth = (width / totalFlex)
-
-    const positions = flexs.reduce((acc, item, i)=>{
-      const currentWidth = flexUnitWidth * item
-      return {
-        lastWidth: acc.lastWidth + currentWidth,
-        p: acc.p.concat(acc.lastWidth + currentWidth / 2)
+    let lastDelta0 = 0
+    let lastDelta1 = 0
+    const newPositions = initialPositions.map((pos, i) => {
+      let newPos0, newPos1
+      if (pos[0] < e.clientX) {
+        newPos0 = pos[0] - leftScale(e.clientX - pos[0])
+        // newPos0 -= lastDelta0
+      } else {
+        newPos0 = pos[0] + rightScale(pos[0] - e.clientX)
+        // newPos0 += lastDelta0
       }
+      lastDelta0 = newPos0 - pos[0]
 
-    }, {lastWidth:0, p:[]})
+      if (pos[1] < e.clientX) {
+        newPos1 = pos[1] - leftScale(e.clientX - pos[1])
+        // newPos1 -= lastDelta1
+      } else {
+        newPos1 = pos[1] + rightScale(pos[1] - e.clientX)
+        // newPos1 += lastDelta1
+      }
+      lastDelta1 = newPos1 - pos[1]
 
+      return [newPos0, newPos1]
 
-    this.flexScale.domain([width, maxFlex*flexUnitWidth, 0])
+    })
 
-    const newFlexs = positions.p.map(pos => this.flexScale(Math.abs(e.clientX - pos ).toFixed(1)))
-    this.setState({flexs:newFlexs})
-
-    // const positions = prevInterpolatedStyles.reduce((acc, item, i)=>{
-    //   const w = i === 0 ? 0 : acc[i-1]
-    //   return acc.concat(w + flexUnitWidth * item.f)
-    // }, [])
-
-
-
-
+    // console.log(newPositions)
+    this.setState({ positions: newPositions })
   }
 
   handleMouseOut = (e) => {
-    this.setState({x: null})
-  }
-
-  getStyles = prevInterpolatedStyles => {
-
-    const { width, x } = this.state
-    const itemWidth = width ? width / NUM_PICS : 0
-    const flexo = (i, positions) => x ? this.flexScale(Math.abs(x - (positions[i] / itemWidth).toFixed(2))) : 1
-
-    const totalFlex = sumBy(prevInterpolatedStyles, item => item.f)
-    const flexUnitWidth = (width / totalFlex).toFixed(2)
-    const positions = prevInterpolatedStyles.reduce((acc, item, i)=>{
-      const w = i === 0 ? 0 : acc[i-1]
-      return acc.concat(w + flexUnitWidth * item.f)
-    }, [])
-
-    console.log("prevInterpolatedStyles", prevInterpolatedStyles, totalFlex)
-    return  prevInterpolatedStyles.map(
-      (_, i) => { return ({f: spring(flexo(i, positions))})}
-    )
+    this.setState({
+      x: null,
+      positions: this.state.initialPositions,
+    })
   }
 
   render() {
-
+    const { positions, initialPositions } = this.state
     const { docs } = this.props
-    const { width, x, flexs } = this.state
-
-    const itemWidth = width ? width / NUM_PICS : 0
-    // Bender Brother
-    const flexo = (i, previousFlexs) => x ? this.flexScale(Math.abs(x - i)) : 1
-
-    // return (
-    //   <StaggeredMotion
-    //     defaultStyles={docs.map((_, i) => ({ f: flexo(i) }))}
-    //     styles={this.getStyles}>
-    //     {interpolatingStyles =>
-    //       <div className="w-100 h-100 d-flex flex-row"
-    //           onMouseOut={this.handleMouseOut}
-    //           onMouseMove={this.handleMouseMove}>
-    //         {interpolatingStyles.map((style, i) =>
-    //           <div className="h-100"
-    //             key={i}
-    //             style={{
-    //               flex: style.f,
-    //               backgroundImage: `url(${docs[i].snapshot})`,
-    //               backgroundSize: 'cover',
-    //
-    //             }}/>
-    //         )}
-    //       </div>
-    //     }
-    //   </StaggeredMotion>
-    // )
-
-    return (<div className="w-100 h-100 d-flex flex-row"
+    return (
+      <div className="w-100 h-100" style={{ position: 'relative' }}
         onMouseOut={this.handleMouseOut}
-        onMouseMove={this.handleMouseMove}>
-      {itemWidth > 0 && flexs.map((flex, i) =>{
-        // const flex = x ? this.flexScale(Math.abs(x - i)) : 1
-        return (
-          // <Motion key={i} defaultStyle={{flex:1}} style={{flex:spring(flex)}}>
-          // {({flex}) => (
-            <div key={i} className="h-100"
+        onMouseMove={this.handleMouseMove}
+        >
+        {positions.map((pos, i) => {
+          return (
+            <Motion
+              key={i}
+              defaultStyle={{
+                left: initialPositions[i][0],
+                width: initialPositions[i][1] - initialPositions[i][0]
+              }}
               style={{
-                flex,
-                // background:this.getColor(i),
-                backgroundImage:this.getImage(i),
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
+                left: spring(positions[i][0]),
+                width: spring(positions[i][1] - positions[i][0]),
+              }}
+              >
+                {({ left, width }) => (
+                  <div key={i} className='h-100' style={{
+                    position: 'absolute',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                    // border: '1px solid white',
+                    // zIndex: i,
+                    // backgroundColor:
+                    // backgroundImage: this.getImage(i),
+                    backgroundImage: `url(${docs[i].snapshot})`,
+                    left,
+                    width,
+                    // left: pos[0],
+                    // width: pos[1] - pos[0],
+                    // right: pos[1]
+                  }} />
+                )}
+              </Motion>
+          )
+        })}
+      </div>
 
-              }}>
-            </div>
-        //   )}
-        // </Motion>
-        )
-      })}
-
-    </div>)
-
-
+    )
   }
 }
