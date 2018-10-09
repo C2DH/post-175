ed59c65bfb4b94b56826b62854da1876582818aa
gffuma <fumagalli.gf@gmail.com>
2018-10-09 16:17:02 +0200

    Adding collection top filters and search

--- src/components/Collection/CollectionFilters.js
+++ src/components/Collection/CollectionFilters.js
@@ -1,11 +1,76 @@
 import React, { PureComponent } from 'react'
+import debounce from 'lodash/debounce'
 import './Collection.css'
 
+const ALL_CATEGORIES = [
+  {
+    name: 'image',
+  },
+  {
+    name: 'video',
+  }
+]
+
 export default class CollectionFilters extends PureComponent {
+  state = {
+    prevLocationKey: this.props.locationKey,
+    searchText: this.props.search
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (state.prevLocationKey !== props.locationKey) {
+      if (state.searchText !== props.search) {
+        return {
+          searchText: props.search,
+          prevLocationKey: props.locationKey,
+        }
+      }
+      return {
+        prevLocationKey: props.locationKey,
+      }
+    }
+    return null
+  }
+
+  handleOnSearchChange = e => {
+    const text = e.target.value
+    this.setState({
+      searchText: text,
+    }, () => {
+      this.debouncedOnSearchChange(text)
+    })
+  }
+
+  debouncedOnSearchChange = debounce(this.props.onSearchChange, 150)
+
   render() {
+    const { categories, onToggleCategory } = this.props
     return (
       <div className='collection-filters'>
-        <div>Categories:</div>
+        <div className='collection-filters-categories'>
+          <div>CATEGORIES:</div>
+          <div className='categories-radios'>
+            {ALL_CATEGORIES.map(cat => (
+              <div className='category-radio' key={cat.name}>
+                <input
+                  onChange={() => onToggleCategory(cat.name)}
+                  type='checkbox'
+                  checked={categories.indexOf(cat.name) !== -1}
+                />
+                <div className='radio-label'>{cat.name}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='collection-filters-search'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Search'
+            value={this.state.searchText}
+            onChange={this.handleOnSearchChange}
+          />
+        </div>
       </div>
     )
   }
--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import qs from 'query-string'
+import get from 'lodash/get'
 import SideMenu from '../components/SideMenu'
 import CollectionFilters from '../components/Collection/CollectionFilters'
 import CollectionList from '../components/Collection/CollectionList'
@@ -16,10 +18,24 @@ import {
   getCollectionsAllFacets,
 } from '../state/selectors'
 
+const parseQsAsList = str => str.split(',').filter(Boolean)
+
 class Collection extends PureComponent {
   componentDidMount() {
+    const queryParams = qs.parse(this.props.location.search)
+    this.loadDocs(this.parseQueryParams(queryParams))
     this.props.loadCollectionFacets()
-    this.props.loadCollectionDocuments()
+  }
+
+  componentDidUpdate(prevProps) {
+    const prevQueryParams = qs.parse(prevProps.location.search)
+    const queryParams = qs.parse(this.props.location.search)
+    if (
+      prevQueryParams.search !== queryParams.search ||
+      prevQueryParams.categories !== queryParams.categories
+    ) {
+      this.loadDocs(this.parseQueryParams(queryParams))
+    }
   }
 
   componentWillUnmount() {
@@ -27,14 +43,65 @@ class Collection extends PureComponent {
     this.props.unloadCollectionFacets()
   }
 
+  loadDocs = ({ categories, search }) => {
+    this.props.loadCollectionDocuments({
+      filters: {
+        type__in: categories.length > 0 ? categories : undefined,
+      },
+      q: search,
+    })
+  }
+
+  parseQueryParams = queryParams => {
+    const search = get(queryParams, 'search', '')
+    const categories = parseQsAsList(get(queryParams, 'categories', ''))
+    return { search, categories }
+  }
+
+  handleOnSearchChange = search => {
+    const { location } = this.props
+    const queryParams = qs.parse(location.search)
+
+    this.props.history.push(`${location.pathname}?${qs.stringify({
+      ...queryParams,
+      search
+    }, {
+      encode: false,
+    })}`)
+  }
+
+  handleToggleCategory = category => {
+    const { location } = this.props
+    const queryParams = qs.parse(location.search)
+    const oldCategories = parseQsAsList(get(queryParams, 'categories', ''))
+    const categories = oldCategories.indexOf(category) === -1
+      ? oldCategories.concat(category)
+      : oldCategories.filter(cat => cat !== category)
+
+    this.props.history.push(`${location.pathname}?${qs.stringify({
+      ...queryParams,
+      categories: categories.join(','),
+    }, {
+      encode: false,
+    })}`)
+  }
+
   render() {
-    const { docs, facets, allFacets } = this.props
-    console.log({ docs, facets, allFacets })
+    const { docs, facets, allFacets, location } = this.props
+    const queryParams = qs.parse(location.search)
+    const { search, categories } = this.parseQueryParams(queryParams)
+
     return (
       <div className='h-100 collection-container'>
         <SideMenu />
         <div className='h-100 with-sidemenu collection'>
-          <CollectionFilters />
+          <CollectionFilters
+            locationKey={location.key}
+            search={search}
+            onSearchChange={this.handleOnSearchChange}
+            categories={categories}
+            onToggleCategory={this.handleToggleCategory}
+          />
           {docs && <CollectionList docs={docs} />}
           <CollectionTimeBrush />
         </div>
@@ -51,4 +118,5 @@ export default connect(state => ({
   loadCollectionDocuments,
   unloadCollectionDocuments,
   loadCollectionFacets,
+  unloadCollectionFacets,
 })(Collection)
