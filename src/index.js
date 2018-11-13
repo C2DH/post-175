1807d839f8a74cbbfbfbc32cdbce9aaaa0ac6151
uf0 <scnepa222@gmail.com>
2018-11-13 15:22:09 +0100

    using sass

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
 import "mapbox-gl/dist/mapbox-gl.css";
-import "./index.css";
+import "./theme/index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
