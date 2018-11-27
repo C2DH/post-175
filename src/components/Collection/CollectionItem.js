e3bc2ae48fee1418dd71bb9ab1e08bc28f2103b9
gffuma <fumagalli.gf@gmail.com>
2018-11-27 11:58:26 +0100

    Style collection

--- src/components/Collection/CollectionItem.js
+++ src/components/Collection/CollectionItem.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import classNames from 'classnames'
 import { Link } from 'react-router-dom'
 import MagikDotDotDot from '../MagikDotDotDot'
 import './Collection.css'
@@ -9,17 +10,19 @@ export default class CollectionItem extends PureComponent {
     return (
       <Link to={{ pathname: `/doc/${doc.id}`, state: { modal: true } }}>
         <div className='collection-item'>
-          <div className='item-image-box'>
-            <img src={doc.snapshot} alt={doc.title} />
-          </div>
           <div className='item-caption'>
+            <div className='item-year'>{doc.data.year}</div>
             <div className='item-label'>
               {/* Fuck off multiline ellipsis use dot dot dot */}
               <MagikDotDotDot clamp={2}>
                 {doc.data.title}
               </MagikDotDotDot>
             </div>
-            <div className='item-year'>{doc.data.year}</div>
+          </div>
+          <div className={classNames('item-image-box', {
+            'item-image-large': doc.large,
+          })}>
+            <img src={doc.snapshot} alt={doc.title} />
           </div>
         </div>
       </Link>
--- src/components/Collection/CollectionList.js
+++ src/components/Collection/CollectionList.js
@@ -11,7 +11,7 @@ export default class CollectionList extends PureComponent {
       <div className='collection-list'>
         <div className='collection-list-wrapper'>
           {docs.map((docRow, i) => (
-            <div className='row collection-row' key={i}>
+            <div className='row no-gutters collection-row' key={i}>
               {docRow.map(doc => (
                 <div key={doc.id} className={classNames(`collection-col`, {
                   'col': docRow.length === 6,
