8c59274fcb13d393d5d33a24717cd005cf78a5d3
uf0 <scnepa222@gmail.com>
2019-01-22 15:47:10 +0100

    terms of use page

---
+++ src/pages/TermsOfUse/TermsOfUse.js
@@ -0,0 +1,57 @@
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import ReactMarkdown from "react-markdown";
+import { localize } from "../../localize";
+import { loadStory, unloadStory } from "../../state/actions";
+import { getStory } from "../../state/selectors";
+import SideMenu from "../../components/SideMenu";
+import "./TermsOfUse.scss";
+
+class TermsOfUse extends PureComponent {
+  componentDidMount() {
+    this.props.loadStory("terms-of-use");
+  }
+
+  componentWillUnmount() {
+    this.props.unloadStory();
+  }
+
+  render() {
+    const { story, t } = this.props;
+    return (
+      <div className="h-100 TermsOfUse bg-white">
+        <SideMenu />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 className="my-4 text-uppercase">
+                {story ? story.data.title : ""}
+              </h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-8">
+              <div className="page-content">
+                {story && <ReactMarkdown source={story.data.abstract} />}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  story: getStory(state)
+});
+
+export default localize()(
+  connect(
+    mapStateToProps,
+    {
+      loadStory,
+      unloadStory
+    }
+  )(TermsOfUse)
+);
