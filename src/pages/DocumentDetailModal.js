942f34e64bb4ae2961e4813a66deeb0d69065f6a
gffuma <fumagalli.gf@gmail.com>
2019-02-22 11:17:25 +0100

    Collection detail modal transitions closes #61

--- src/pages/DocumentDetailModal.js
+++ src/pages/DocumentDetailModal.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
+import { Motion, spring, TransitionMotion } from "react-motion"
 import { connect } from 'react-redux'
-import find from 'lodash/find'
 import {
   getCollectionDocumentDetail,
 } from '../state/selectors'
@@ -9,9 +9,12 @@ import {
   unloadDocument,
 } from '../state/actions'
 import DocDetail from '../components/DocDetail'
-import SideMenu from '../components/SideMenu'
 
 class DocumentDetailModal extends PureComponent {
+  state = {
+    closing: false,
+  }
+
   componentDidMount() {
     this.props.loadDocument(this.props.match.params.id)
   }
@@ -21,27 +24,65 @@ class DocumentDetailModal extends PureComponent {
   }
 
   onClose = () => {
-    this.props.history.goBack()
+    this.setState({ closing: true })
   }
 
   render() {
+    const { closing } = this.state
     const { doc } = this.props
 
     return (
-      <div style={{
-        position: 'fixed',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        zIndex: 9999,
-        height: '100%',
-      }}>
-        {doc && <DocDetail
-          doc={doc}
-          onClose={this.onClose}
-        />}
-      </div>
+      <TransitionMotion
+        defaultStyles={
+          !closing
+            ? [
+                {
+                  key: "modal",
+                  style: { o: 0 }
+                }
+              ]
+            : []
+        }
+        styles={
+          !closing
+            ? [
+                {
+                  key: "modal",
+                  style: { o: spring(1) }
+                }
+              ]
+            : []
+        }
+        willLeave={() => ({ o: spring(0) })}
+        willEnter={() => ({ o: 0 })}
+        didLeave={() => {
+          if (this.state.closing) {
+            this.props.history.goBack()
+          }
+        }}
+      >
+        {interpolatedStyles => (
+          <div>
+            {interpolatedStyles.map(config => (
+              <div key='doc-detail' style={{
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                zIndex: 9999,
+                opacity: config.style.o,
+                height: '100%',
+              }}>
+                {doc && <DocDetail
+                  doc={doc}
+                  onClose={this.onClose}
+                />}
+              </div>
+            ))}
+          </div>
+        )}
+      </TransitionMotion>
     )
   }
 }
