e9572132da422e207a2015f9209ccd4e79e4632d
uf0 <scnepa222@gmail.com>
2019-10-29 17:45:57 +0100

    add GA code in .env -> see .env.example, closes #79

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import I18n from 'redux-i18n'
-import { BrowserRouter as Router } from 'react-router-dom'
-import store from './state'
-import Root from './components/Root'
-import translations from './translations'
+import React from "react";
+import { Provider } from "react-redux";
+import I18n from "redux-i18n";
+import { createBrowserHistory } from "history";
+import ReactGA from "react-ga";
+import { Router } from "react-router-dom";
+import store from "./state";
+import Root from "./components/Root";
+import translations from "./translations";
+
+const history = createBrowserHistory();
+
+// integrate history \w Google Analytics
+if (process.env.REACT_APP_GA_CODE) {
+  ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+  ReactGA.pageview(history.location.pathname + history.location.search);
+
+  history.listen((location, action) => {
+    ReactGA.pageview(location.pathname + location.search);
+  });
+}
 
 const App = () => (
   <Provider store={store}>
-    <I18n translations={translations} initialLang='fr'>
-      <Router>
+    <I18n translations={translations} initialLang="fr">
+      <Router history={history}>
         <Root />
       </Router>
     </I18n>
   </Provider>
-)
+);
 
-export default App
+export default App;
